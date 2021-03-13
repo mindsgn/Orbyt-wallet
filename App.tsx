ddc1ce43486b753760e7f8dbdb06fecba61fa631
mindsgn <seni.tembe@protonmail.com>
2021-03-13 20:47:52 +0200

    web3 setup

--- App.tsx
+++ App.tsx
@@ -5,12 +5,24 @@
  * @format
  * @flow strict-local
  */
+ import "./shim"
+import './global';
 import React from 'react';
+import { Text, View } from 'react-native';
+import Web3 from 'web3';
+
 
 function App(){
+  const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
+  const account = web3.eth.accounts.create();
+
+  console.log();
+
   return (
-    <>
-    </>);
+    <View>
+      <Text>{account.address}</Text>
+    </View>);
 };
 
 export default App;
