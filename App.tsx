35b17c30d194a1d71796808b3b6441e13c554b02
mindsgn <seni.tembe@protonmail.com>
2021-04-07 19:32:51 +0200

    fix store configuration

--- App.tsx
+++ App.tsx
@@ -5,28 +5,20 @@
  * @format
  * @flow strict-local
  */
-import "./shim"
+import "./shim";
 import './global';
-
 import React from 'react';
-import Web3 from 'web3';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
+import Home from './view/home';
+const store = configureStore();
 
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import Home from './view/home'
-
-
-function App(){
-  const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-
-  const account = web3.eth.accounts.create();
-
-  console.log();
-
+const App = () => {
   return (
     <Provider store={store}>
       <Home />
-    </Provider>);
+    </Provider>
+  );
 };
 
 export default App;
