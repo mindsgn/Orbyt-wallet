dc38681449bd4d23d69db00fa48072f14a86a6f4
mindsgn <seni.tembe@protonmail.com>
2021-04-07 21:58:26 +0200

    new component for logo

---
+++ style/logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styled from 'styled-components/native';
+
+const Text = Styled.Text`
+  color: white;
+  font-size: 42;
+  font-weight: bold;
+`;
+
+export default Text;
