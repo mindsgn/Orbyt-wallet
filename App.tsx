e0e2226b7fe876380756a635445bddfe5c100194
mindsgn <seni.tembe@protonmail.com>
2021-04-07 21:56:06 +0200

    new screen & stack navigation for navigation

--- App.tsx
+++ App.tsx
@@ -1,22 +1,43 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+  * Sample React Native App
+  * https://github.com/facebook/react-native
+  *
+  * @format
+  * @flow strict-local
+  **/
+
 import "./shim";
 import './global';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 import Home from './view/home';
+import Load from './view/load';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 const store = configureStore();
+const Stack = createStackNavigator();
 
 const App = () => {
-  return (
+  return (    
     <Provider store={store}>
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/*screen loading*/}
+          <Stack.Screen
+            name="Load"
+            component={Load}
+            options={{headerShown:false}}/>
+            
+          {/*screen home*/}
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{headerShown:false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
