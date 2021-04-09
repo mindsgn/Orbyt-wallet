49d7628ce0b2818ad447e2913cab1800aaece754
mindsgn <seni.tembe@protonmail.com>
2021-04-09 16:25:29 +0200

    added redux store.tsx

---
+++ redux/store.tsx
@@ -0,0 +1,14 @@
+import { createStore, combineReducers } from 'redux';
+import AccountReducer from './reducer';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+  states: AccountReducer
+});
+
+const configureStore = () => {
+  return createStore(rootReducer, applyMiddleware(thunk));
+}
+
+export default configureStore;
