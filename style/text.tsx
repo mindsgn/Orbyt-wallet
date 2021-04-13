afa8a5993c4960cf90dd3e1911455c5ef2a7b116
mindsgn <seni.tembe@protonmail.com>
2021-04-13 19:49:06 +0200

    styled components text

---
+++ style/text.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Styled from 'styled-components/native';
+
+interface Props{
+  background: string
+}
+
+const Text= Styled.Text`
+  color: black;
+  font-size: 21;
+`;
+
+export default Text;
