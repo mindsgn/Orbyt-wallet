2f3a48303b099e17b35a50e35fe43e29b754a562
mindsgn <seni.tembe@protonmail.com>
2021-04-13 12:45:30 +0200

    updated actions: database and web3

--- redux/action.tsx
+++ redux/action.tsx
@@ -1,14 +1,63 @@
-import axios from 'axios';
-import { GET_ACCOUNT, ERROR } from './constants';
-import RNFS from 'react-native-fs';
+import Web3 from 'web3';
+import { ACCOUNT } from './constants';
+const web3 = new Web3("ws://localhost:7545");
+import realm from "../data/database";
+const { v4: uuidv4 } = require('uuid');
 
-//create new account
+//check if user has stored account
 export function getAccount() {
+  return async (dispatch) => {
+    try{
+      const data = realm.objects('User');
+      if(data.length == 0){
+        let response = {
+          account: false
+        }
+        dispatch(updateAccount(response));
+      }else{
+        let response = {
+          account: true
+        }
+        dispatch(updateAccount(response));
+      }
+    }
+    catch (error) {
+      let response = {
+        isauth:false,
+        error: true
+      }
+      dispatch(updateAccount(response));
+    }
+  };
+}
+
+//check if user has stored account
+export function createAccount() {
   return async (dispatch) => {
     try{
     }
     catch (error) {
-      console.log(error);
     }
   };
 }
+
+//Delete Account From user
+export function deleteAccount() {
+  return async (dispatch) => {
+    try{
+    }
+    catch (error) {
+    }
+  }
+}
+
+export function updateAccount(data) {
+  return {
+    type: ACCOUNT,
+    payload: data,
+  };
+}
+
+
+
+
